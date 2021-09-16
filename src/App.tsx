c3813cf355f0939dd610667ec844d67896567f99
Devan Grose <devan.grose@crowdstreet.com>
2021-09-16 14:17:33 -0700

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
+        <Link to="/counter">Counter Link</Link>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,25 @@ import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
+import { Counter } from './features/counter/Counter';
+
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <App />
+          </Route>
+          <Route path="/counter">
+            <Counter />
+          </Route>
+        </Switch>
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
