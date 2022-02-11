537e78b54f6aca396908ccd124dbf29c13e11304
Patrick McWiliams <patrick@agency73.com>
2022-02-11 04:53:41 -0800

    added service with fetch stub and loading indicator

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from './Home.module.css';
-import { Box, Grid, GridSize } from '@material-ui/core';
+import { Box, Grid, GridSize, Typography } from '@material-ui/core';
 import ApprovalForm from '../../components/ApprovalForm/ApprovalForm';
 import SubmitButton from '../../components/SubmitButton/SubmitButton';
 
@@ -19,6 +19,11 @@ const Home = () => {
         <Box display="flex" alignItems='center' justifyContent="flex-end">
           <SubmitButton/>
         </Box>
+      </Grid>
+      <Grid item className={styles.Content} xs={10} lg={8}>
+        <Typography>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Maecenas sed enim ut sem viverra aliquet eget. Vel facilisis volutpat est velit egestas dui id ornare. Sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum curabitur. Vel turpis nunc eget lorem. Eu nisl nunc mi ipsum faucibus vitae aliquet nec ullamcorper. Non enim praesent elementum facilisis leo vel fringilla est. Proin libero nunc consequat interdum varius sit amet. Nulla posuere sollicitudin aliquam ultrices. Mauris rhoncus aenean vel elit scelerisque mauris pellentesque pulvinar pellentesque. Arcu non sodales neque sodales.
+        </Typography>
       </Grid>
     </Grid>
   )
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,12 +1,14 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import errorReducer from './reducers/ErrorSlice';
 import approvalFormReducer from './reducers/ApprovalFormSlice';
+import loadingReducer from './reducers/LoadingSlice';
 import logger from 'redux-logger';
 
 export const store = configureStore({
   reducer: {
     errors: errorReducer,
-    approvalForm: approvalFormReducer
+    approvalForm: approvalFormReducer,
+    loading: loadingReducer,
   },
   middleware: [logger]
 });
